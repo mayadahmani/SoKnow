416b55e8d3f025aca6e4f9b7ff07a45164c63c63
mayadahmani <dahmanimaya68@gmail.com>
2026-03-25 17:01:33 +0100

    autolaoder

--- config/autoloader.php
+++ config/autoloader.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * SoKnow Autoloader
- * Automatically loads classes from the /models folder
- */
+
 spl_autoload_register(function ($class) {
     $directory = __DIR__ . '/../models/';
 $file = $directory . $class . '.php';
